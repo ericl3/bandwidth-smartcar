3bf1d62fea2286185ae926e659e5af5ac2d650fd
Eric <e.lin1331@gmail.com>
2019-05-28 11:08:03 -0400

    updated smartcar redirect URI

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 module.exports = {
     cliendId: "2a774cd6-bd8d-41ce-ba90-638cd1419625",
     clientSecret: "0fa28e25-5831-49ef-bba9-4080d54b959e",
-    redirectUri: "http://localhost:8080/connect/exchange",
+    redirectUri: "https://bw-smartcar.herokuapp.com/connect/exchange",
     buserId: "u-zywoyl33pqw2awx4su6z3aa",
     bapiToken: "t-rn2act3hflg4lt6oprm5rey",
     bapiSecret: "tiitdtqjmbiaj3be3uqkwcj5ukazhvs7lhvckya",
